bb9e0a92afbb73d14edb7e17dd454bde17edecaa
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2023-01-07 16:40:26 +0300

    Update ThiefCheck.cs

--- AlarmForThief/ThiefCheck.cs
+++ AlarmForThief/ThiefCheck.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class ThiefCheck : MonoBehaviour
+public class ThiefChecker : MonoBehaviour
 {
-    [SerializeField] private UnityEvent _reached;
+    [SerializeField] private UnityEvent _Entered;
+    [SerializeField] private UnityEvent _CameOut;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out Player player))
         {
-            _reached?.Invoke();
+            _Entered?.Invoke();
         }
     }
 
@@ -20,7 +20,7 @@ public class ThiefCheck : MonoBehaviour
     {
         if (collision.TryGetComponent(out Player player))
         {
-            _reached?.Invoke();
+            _CameOut?.Invoke();
         }
     }
 }
